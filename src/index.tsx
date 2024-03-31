e24bc62151dd23b2b0299e7854e9914548f88c60
Nurmy <nurmy26@yandex.ru>
2024-03-31 17:58:02 +0300

    fix: deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import "./index.css";
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
@@ -12,9 +12,9 @@ import store from "./services/store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
